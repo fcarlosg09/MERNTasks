576989cbe2743ecf6c628b87a70a09a561a62220
Carlos Fonseca <carlosfonsecag09@gmail.com>
2020-06-01 17:33:01 -0500

    Front de componentes Login y Nueva Cuenta

--- cliente/src/Components/auth/Login.js
+++ cliente/src/Components/auth/Login.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
 
+    const [infoUsuario, setinfoUsuario] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [camposincompletos, setcamposincompletos] = useState(false);
+
+    const { email, password } = infoUsuario;
+
     const onChangeInicioSesion = (e) =>{
-        console.log(e.target.value);
+        setinfoUsuario({
+            ...infoUsuario,
+            [e.target.name] : e.target.value
+        })
+    };
+
+    const FnInicioSesion = (e) =>{
+        e.preventDefault();
+        if(email.trim() === '' || password.trim() === ''){
+            camposincompletos(false); return;
+        }
+        camposincompletos(true);
+
+        //Action
     }
 
     return ( 
         <div className="form-usuario">
             <div className="contenedor-form sobra-dark">
                 <h1>Iniciar Sesión</h1>
-                <form>
+                <form
+                    onSubmit={FnInicioSesion}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input
@@ -19,9 +45,32 @@ const Login = () => {
                             name="email"
                             placeholder="Tu email"
                             onChange={onChangeInicioSesion}
+                            value={email}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={onChangeInicioSesion}
+                            value={password}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesión"
                         />
                     </div>
                 </form>
+
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
             </div>
         </div>
      );
--- cliente/src/Components/auth/NuevaCuenta.js
+++ cliente/src/Components/auth/NuevaCuenta.js
@@ -1,8 +1,101 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+    const [infoNuevoUsuario, setinfoNuevoUsuario] = useState({
+        nombre: '',
+        email: '',
+        password: '',
+        passwordConfirmar: ''
+    })
+
+    const { nombre, email, password, passwordConfirmar} = infoNuevoUsuario;
+
+    const onChangeRegistro = (e) =>{
+        setinfoNuevoUsuario({
+            ...infoNuevoUsuario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const FnRegistroNuevoUsuario = (e) =>{
+        e.preventDefault();
+
+        //Validar que no haya campos vacios
+
+
+        //Validar Password mínimo de 6 caracteres
+
+        //Los 2 Password son iguales
+
+        //Action
+    }
+
     return ( 
-        <h1>Desde NuevaCuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sobra-dark">
+            <h1>Obtener una cuenta</h1>
+            <form
+                onSubmit={FnRegistroNuevoUsuario}
+            >
+                <div className="campo-form">
+                    <label htmlFor="nombre">Nombre</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        name="nombre"
+                        placeholder="Tu nombre"
+                        onChange={onChangeRegistro}
+                        value={nombre}
+                    />
+                </div>
+                <div className="campo-form">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Tu email"
+                        onChange={onChangeRegistro}
+                        value={email}
+                    />
+                </div>
+                <div className="campo-form">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Tu password"
+                        onChange={onChangeRegistro}
+                        value={password}
+                    />
+                </div>
+                <div className="campo-form">
+                    <label htmlFor="password">Confirmar Password</label>
+                    <input
+                        type="password"
+                        id="passwordConfirmar"
+                        name="passwordConfirmar"
+                        placeholder="Repite tu password"
+                        onChange={onChangeRegistro}
+                        value={passwordConfirmar}
+                    />
+                </div>
+                <div className="campo-form">
+                    <input 
+                        type="submit"
+                        className="btn btn-primario btn-block"
+                        value="Registrarme"
+                    />                
+                </div>
+                <Link to="/" className="enlace-cuenta">
+                    Volver a iniciar sesión
+                </Link>
+            </form>
+            </div>
+        </div>
      );
 }
  
